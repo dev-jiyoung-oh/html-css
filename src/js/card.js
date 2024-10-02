d7b36c4f5b45d92cdf11cb2f310dac65bfb271a9
jiyoung <aa991102@naver.com>
2024-10-02 17:02:42 +0900

    script 요소

---
+++ src/js/card.js
@@ -0,0 +1,5 @@
+let button = document.querySelector(".flip-button");
+let card = document.querySelector(".card");
+button.addEventListener("click", function () {
+  card.classList.toggle("is-flipped");
+});
