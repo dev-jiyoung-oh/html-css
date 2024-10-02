7438fc03df5fd4454ffbd153ab726ce7e4a9ca95
jiyoung <aa991102@naver.com>
2024-10-02 17:04:37 +0900

    유저 인터렉션 속성

---
+++ src/js/peak-time.js
@@ -0,0 +1,7 @@
+const button = document.querySelector(".voting-result-button");
+const results = document.querySelectorAll(".voting-result");
+button.addEventListener("click", () => {
+  results.forEach((result) => {
+    result.removeAttribute("hidden");
+  });
+});
